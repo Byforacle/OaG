a2b434edf02fae9c7d5b94b0a1f72b21854a6d55
Byforacle <2523920747@qq.com>
2026-02-10 22:44:15 +0800

    Initial Commit

---
+++ OaG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OaG
+//
+//  Created by byf on 2026/2/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OaG/OaGApp.swift
@@ -0,0 +1,17 @@
+//
+//  OaGApp.swift
+//  OaG
+//
+//  Created by byf on 2026/2/10.
+//
+
+import SwiftUI
+
+@main
+struct OaGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
